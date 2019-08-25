e33cd9f82860ca5628fc427c025d8dd0431ef9f7
Raylex Lee <raylexym57lee@gmail.com>
2019-08-25 23:51:21 +0800

    Split for title and caption for 287

--- getpodcast.js
+++ getpodcast.js
@@ -2,9 +2,10 @@ const syncRequest = require('sync-request');
 const xpath = require('xpath');
 const dom = require('xmldom').DOMParser;
 const fs = require('fs');
+const split = require('./lib/split.js');
 
-const episodesUrl = (pid, year, page) =>  
-  `https://podcast.rthk.hk/podcast/episodeList.php?pid=${pid}&year=${year}&page=${page}`; 
+const episodesUrl = (pid, year, p) =>  
+  `https://podcast.rthk.hk/podcast/episodeList.php?pid=${pid}&year=${year}&page=${p}`; 
 const yearsUrl = pid => `https://podcast.rthk.hk/podcast/item.php?pid=${pid}`;  
 
 const Pid = '287';
@@ -19,21 +20,23 @@ const years = pid => syncRequest('GET', yearsUrl(pid))
 console.log(ProgOf[Pid]);
 years(Pid)
   .forEach(year => {
-    let page = 0, remainder = '99';
+    let p = 0, remainder = '99';
     do {
-      page = page + 1;
-      const res = syncRequest('GET', episodesUrl(Pid, year, page));
+      p = p + 1;
+      const res = syncRequest('GET', episodesUrl(Pid, year, p));
       const xml = res.getBody('utf8');
       const doc = new dom().parseFromString(xml);
       remainder = xpath.select1('/episodeList/remainder', doc).firstChild.data;
-      const titles = xpath.select('//episodeTitle', doc);
+      const titleCaptions = xpath.select('//episodeTitle', doc);
       const dates = xpath.select('//episodeDate', doc);
       const audios = xpath.select('//mediafile', doc);
-      for (let i = 0; i < titles.length; i++) {
-        const title = titles[i].firstChild.data;
+      for (let i = 0; i < titleCaptions.length; i++) {
+      //  const titleCaption = titleCaptions[i].firstChild.data;
+        const [title, caption] = split(titleCaptions[i].firstChild.data, Pid);
         const date = dates[i].firstChild.data;
         const audio = audios[i].firstChild.data;
-        console.log(date, ' ', title, ' ', audio);
+        console.log(date, ' ', title, ' --> ', caption, ' ', audio);
+      //  console.log(date, ' ', titleCaption, ' ', audio);
       }
 
     } while (remainder !== '0');
