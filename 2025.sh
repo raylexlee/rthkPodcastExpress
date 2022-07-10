32821d936bfe0e12de358d262d2168edec826202
Raylex Lee <raylexym57lee@gmail.com>
2022-07-10 22:48:12 +0800

    Update entries

--- 2025.sh
+++ 2025.sh
@@ -5,11 +5,11 @@ curl -s -o 我愛你愛電視劇/01編劇陳韻文\(上\).mp3 https://podcasts.r
 curl -s -o 我愛你愛電視劇/02編劇陳韻文\(下\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111123_73078.mp3
 curl -s -o 我愛你愛電視劇/03輪流傳.mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111126_45522.mp3
 curl -s -o 我愛你愛電視劇/04鄭裕玲\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111127_61291.mp3
-curl -s -o 我愛你愛電視劇/05鄭裕玲\(下\) + 葉潔馨\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111129_15324.mp3
+curl -s -o 我愛你愛電視劇/05鄭裕玲\(下\)\ +\ 葉潔馨\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111129_15324.mp3
 curl -s -o 我愛你愛電視劇/06葉潔馨\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111130_97157.mp3
-curl -s -o 我愛你愛電視劇/07葉潔馨\(下\) + 王晶\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111132_31673.mp3
+curl -s -o 我愛你愛電視劇/07葉潔馨\(下\)\ +\ 王晶\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111132_31673.mp3
 curl -s -o 我愛你愛電視劇/08王晶\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111135_78607.mp3
-curl -s -o 我愛你愛電視劇/09王晶\(下\) + 李司棋\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111137_33930.mp3
+curl -s -o 我愛你愛電視劇/09王晶\(下\)\ +\ 李司棋\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111137_33930.mp3
 curl -s -o 我愛你愛電視劇/10李司棋\(下\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111139_17031.mp3
 curl -s -o 我愛你愛電視劇/11黃淑儀\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111142_66907.mp3
 curl -s -o 我愛你愛電視劇/12黃淑儀\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2205111143_68616.mp3
@@ -20,3 +20,6 @@ curl -s -o 我愛你愛電視劇/16周梁淑怡\(上\).mp3 https://podcasts.rthk
 curl -s -o 我愛你愛電視劇/17周梁淑怡\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206011600_96620.mp3
 curl -s -o 我愛你愛電視劇/18周梁淑怡\(下\)+李香琴\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206071715_48400.mp3
 curl -s -o 我愛你愛電視劇/19李香琴\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206081752_57234.mp3
+curl -s -o 我愛你愛電視劇/20李香琴\(下\)+許鞍華\(上\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206271502_99593.mp3
+curl -s -o 我愛你愛電視劇/21許鞍華\(中\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206271509_21971.mp3
+curl -s -o 我愛你愛電視劇/22許鞍華\(下\).mp3 https://podcasts.rthk.hk/podcast/media/welovetvdrama/2025_2206291204_48849.mp3
