8ff575cbbbecf4484ad1bf96be0a305609e36d5d
Raylex Lee <raylexym57lee@gmail.com>
2023-08-29 14:49:21 +0800

    Bash script to get progamme list

---
+++ getprogof.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+REMAINDER=99
+PAGE=3
+while [ "$REMAINDER" != "0" ]
+do
+  LIST_URL='https://podcast.rthk.hk/podcast/programmeList.php?type=audio&page='"$PAGE"'&order=stroke&lang=zh-CN'
+  curl -s -o temp.xml $LIST_URL
+  PAGE=$(expr $PAGE + 1)
+  REMAINDER=$(xmlstarlet sel -t -v '/programmeList/remainder' temp.xml)
+  PIDS=($(xmlstarlet sel -t -v '//link' temp.xml | sed 's/.*=\(.*\)/\1/'))
+  TITLES=($(xmlstarlet sel -t -v '//title' temp.xml | sed 's/ /_/g'))
+  LAST=$(expr ${#PIDS[@]} - 1)
+  for I in $(seq 0 $LAST)
+  do
+    echo ${PIDS[$I]} ${TITLES[$I]}
+  done
+done
