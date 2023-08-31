8060f041ac29f762d45d3acfb0fd6328dce57f9e
Raylex Lee <raylexym57lee@gmail.com>
2023-08-31 21:05:56 +0800

    Remove un-needed script

--- update_podcast.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-PROG_CODE=${1?"Missing programe code such as 287 or 2025 ..."}
-rm $PROG_CODE*
-./getpodcast.js $PROG_CODE 
-./writehtmls.js $PROG_CODE 
-./writeindexpage.js $PROG_CODE   
-./write_curl_audio_sh.js $PROG_CODE  
