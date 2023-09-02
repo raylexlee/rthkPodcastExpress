87369fcddf30d1e2d51eb3f9363724f73d286348
Raylex Lee <raylexym57lee@gmail.com>
2023-09-02 20:28:03 +0800

    Refine the workflow.

--- refresh_podcast.sh
+++ refresh_podcast.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 PROG_CODE=${1?"Missing programe code such as 287 or 2025 ..."}
-rm "$PROG_CODE".json
-./getPodcast.js $PROG_CODE 
-mv $PROG_CODE.json $HOME/raylexlee.github.io/myrthk/
+./newpodcastsSinceLastUpdate.sh $PROG_CODE
+case $? in
+0)
+    ./getPodcast.js $PROG_CODE 
+    mv $PROG_CODE.json $HOME/raylexlee.github.io/myrthk/
+    cd $HOME/raylexlee.github.io/myrthk/
+    git add $PROG_CODE.json
+    git commit -m "Update programme"
+    git push origin master
+    ;;
+1)
+    echo "No new episodes yet!"
+    ;;
+2)
+    echo "No such programe id $PROG_CODE"
+    ;;
+*)
+    echo "failed with unknow status."
+    ;;
+esac
