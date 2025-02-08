8493fe54eae16c9c52c922e002649eebc8ac6b2a
Raylex Lee <raylexym57lee@gmail.com>
2025-02-08 23:25:21 +0800

    Remove html escape entities

--- people/getContentFromTagMetaDescription.sh
+++ people/getContentFromTagMetaDescription.sh
@@ -4,5 +4,5 @@ PID=287
 EPISODES_URL='https://podcast.rthk.hk/podcast/item.php?pid='"$PID"'&eid='"$EID"
 curl -s -o temp.html $EPISODES_URL
 dos2unix -q temp.html
-cat temp.html | pup 'meta[name="description"] attr{content}'
+cat temp.html | pup 'meta[name="description"] attr{content}' | sed 's/\&[amp;]*\#[0-9]*;//g'
 rm temp.html
