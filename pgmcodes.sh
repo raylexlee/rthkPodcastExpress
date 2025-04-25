bb5928b85c6ad8ff7fe95334f7d31819465cc624
Raylex Lee <raylexym57lee@gmail.com>
2025-04-25 14:03:02 +0800

    Batch update rthk podcasts.

---
+++ pgmcodes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+./refresh_podcast.sh 287
+./refresh_podcast.sh 1223
+./refresh_podcast.sh 527
+./refresh_podcast.sh 1632
+./refresh_podcast.sh 2034
+./refresh_podcast.sh 1069
+./refresh_podcast.sh 1581
+./refresh_podcast.sh 2404
+cd $HOME/raylexlee.github.io/myrthk/
+./genProgrammesJS.sh.txt
